9add8d91e4ce303cf789881d51bbb600d62d0105
Stone <stone.dummett@gmail.com>
2021-07-28 23:12:53 +0200

    Some unix process functions test

--- unix_process/string_through_pipe.c
+++ unix_process/string_through_pipe.c
@@ -5,32 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/28 01:02:32 by sdummett          #+#    #+#             */
-/*   Updated: 2021/07/28 01:13:24 by sdummett         ###   ########.fr       */
+/*   Created: 2021/07/28 19:46:48 by sdummett          #+#    #+#             */
+/*   Updated: 2021/07/28 20:05:01 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
 
 int main()
 {
-	int pid = fork();
-	if (pid == -1)
+	int fd[2];
+	if (pipe(fd) == -1)
 		return 1;
-		
+
+	int pid = fork();
+	if (pid < 0)
+		return 2;
+	
 	if (pid == 0)
 	{
-		execlp("ping", "ping", "-c", "3", "google.com", NULL);
-		printf("THIS NOT PRINT ON THEN TERMINAL!\n");
+		//Child process
+		char str[200];
+		printf("Input string: ");
+		fgets(str, 200, stdin);
+		
+		int len = strlen(str) + 1;
+		str[len - 1] = '\0';
+		write(fd[1], &len, sizeof(int));
+		write(fd[1], str, strlen(str) + 1);
 	}
 	else
 	{
+		// Parent process
+		close(fd[1]);
+		int len;
+		char str[200];
+		read(fd[0], &len, sizeof(int));
+		read(fd[0], &str, len);
+		printf("Received: %s", str);
+		close(fd[0]);
 		wait(NULL);
-		printf("Succes !\n");
-		printf("Some post processing goes here\n");
 	}
-	
-	return (0);
+	return 0;
 }
