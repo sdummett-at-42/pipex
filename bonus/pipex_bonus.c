1483664981d614d52c6531f823a9569720fd78d6
Stone <stone.dummett@gmail.com>
2021-08-09 03:30:50 +0200

    Added execve, pipex handles multiple commands

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,17 +6,18 @@
 /*   By: stone <stone@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 22:52:43 by stone             #+#    #+#             */
-/*   Updated: 2021/08/09 00:52:14 by stone            ###   ########.fr       */
+/*   Updated: 2021/08/09 03:29:43 by stone            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-int	exec_cmd(int pipein, int pipeout)
+int	exec_cmd(int pipein, int pipeout, char *cmd)
 {
-	int	value;
 	int fdin;
 	int fdout;
+	char *tab[2]; // <- temporary
+
 	fdin = dup2(pipein, STDIN_FILENO);
 	close(pipein);
 	if (fdin < 0)
@@ -24,11 +25,6 @@ int	exec_cmd(int pipein, int pipeout)
 		perror("dup2");
 		return(1);
 	}
-	read(fdin, &value, sizeof(int));
-	close(fdin);
-	printf("Hello i'm a child process\n");
-	printf("The value read is %d\n", value);
-	value = value + 1;
 	fdout = dup2(pipeout, STDOUT_FILENO);
 	close(pipeout);
 	if (fdout < 0)
@@ -36,29 +32,43 @@ int	exec_cmd(int pipein, int pipeout)
 		perror("dup2");
 		return (2);
 	}
-	write(fdout, &value, sizeof(int));
+	tab[0] = cmd;
+	tab[1] = NULL;
+	fflush(stdout);
+	execve(cmd, tab, NULL);
+	perror(cmd);
 	close(fdout);
-	return (0);
+	return (2);
 }
 
-int main()
+int main(int ac, char **av)
 {
 	int	pipefd1[2];
 	int pipefd2[2];
+	int	infilefd;
+	int	outfilefd;
 	int	pid;
 	int	i;
-	int x = 42;
 
-	if (pipe(pipefd1) < 0)
+	if (ac < 5)
 	{
-		perror("pipe");
+		printf("Error : Wrong number of args.\n");
 		return (1);
 	}
-	write(pipefd1[1], &x, sizeof(int));
-	close(pipefd1[1]);
-
-	i = 0;
-	while (i < 5)
+	infilefd = open(av[1], O_RDONLY);
+	if (infilefd < 0)
+	{
+		perror("open");
+		return (3);
+	}
+	outfilefd = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (outfilefd < 0)
+	{
+		perror("open");
+		return(3);
+	}
+	i = 1;
+	while (i < ac - 2)
 	{
 		pipe(pipefd2);
 		if (pipefd2 < 0)
@@ -73,13 +83,22 @@ int main()
 			return (2);
 		}
 		if (pid == 0)
-			return (exec_cmd(pipefd1[0], pipefd2[1]));
+		{
+			if (i == 1)
+				return (exec_cmd(infilefd, pipefd2[1], av[i + 1]));
+			else if (i == ac - 3)
+				return (exec_cmd(pipefd1[0], outfilefd, av[i + 1]));
+			else
+				return (exec_cmd(pipefd1[0], pipefd2[1], av[i + 1]));
+		}
 		close(pipefd1[0]);
 		close(pipefd2[1]);
 		pipefd1[0] = pipefd2[0];
 		i++;
 	}
-	while (i > 0)
+	close(infilefd);
+	close(outfilefd);
+	while (i > 1)
 	{
 		wait(NULL);
 		i--;
